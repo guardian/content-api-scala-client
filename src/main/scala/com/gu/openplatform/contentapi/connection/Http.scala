430c52d0c10a11faa6ed5e0c2bfd16a95d80447f
stephen.wells <stephen.wells@guardian.co.uk>
2010-10-07 11:22:14 +0100

    added multithreaded apache http trait
    this is useful for certain uses of the client lib.

--- src/main/scala/com/gu/openplatform/contentapi/connection/Http.scala
+++ src/main/scala/com/gu/openplatform/contentapi/connection/Http.scala
@@ -3,10 +3,10 @@ package com.gu.openplatform.contentapi.connection
 import java.lang.String
 
 import org.apache.commons.httpclient.methods.GetMethod
-import org.apache.commons.httpclient.HttpClient
 import com.gu.openplatform.contentapi.ApiError
 import java.net.{URL, HttpURLConnection}
 import io.{Codec, Source}
+import org.apache.commons.httpclient.{MultiThreadedHttpConnectionManager, HttpClient}
 
 case class HttpResponse(body: String, statusCode: Int, statusMessage: String)
 
@@ -16,7 +16,8 @@ trait Http {
 }
 
 
-// an implementtation using apache http client
+// an implementation using apache http client, not this just uses the default connection manager
+// and does not support multithreaded use.
 trait ApacheHttpClient extends Http {
   var httpClient = new HttpClient
 
@@ -36,6 +37,39 @@ trait ApacheHttpClient extends Http {
     method.releaseConnection
 
     new HttpResponse(responseBody, statusLine.getStatusCode, statusLine.getReasonPhrase)
+  }
+}
+
+// an implementation using the MultiThreadedHttpConnectionManager
+// note this defaults to 2 connections in accordance to the http spec
+// feel free to up this number by calling maxConnections
+trait MultiThreadedApacheHttpClient extends Http {
+
+  var connectionManager = new MultiThreadedHttpConnectionManager
+  var httpClient = new HttpClient(connectionManager)
+  
+  def maxConnections(i: Int) {
+    connectionManager.getParams.setMaxTotalConnections(i)
+    connectionManager.getParams.setDefaultMaxConnectionsPerHost(i)
+  }
+
+  def GET(url: String, headers: Iterable[ (String, String) ] = Nil): HttpResponse = {
+    val method = new GetMethod(url)
+    try {
+
+      headers.foreach { case (k, v) => method.addRequestHeader(k, v) }
+
+      httpClient.executeMethod(method)
+
+      val statusLine = method getStatusLine
+      val responseBody = Option(method.getResponseBodyAsStream)
+              .map(Source.fromInputStream(_).mkString)
+              .getOrElse("")
+
+      new HttpResponse(responseBody, statusLine.getStatusCode, statusLine.getReasonPhrase)
+    } finally {
+      method.releaseConnection
+    }
   }
 }
 
