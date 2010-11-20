f468743466037a156551d4e1ff1e130fc6103d5a
Graham Tackley <graham@tackley.net>
2010-11-20 20:47:37 +0000

    Support cross compile for 2.8.0 and 2.8.1. Enable snapshot releases.
    See http://code.google.com/p/simple-build-tool/wiki/CrossBuild for
    information on cross building in sbt.  This project defaults to
    just building for 2.8.1 now, prefix any command with + to build
    for 2.8.0 and 2.8.1.
    
    So you'll probably want to do +update and +publish (or +publish-local)
    from now on to publish.
    
    I've also added a hack to the project file to enable publishing
    snapshot releases to the guardian repo, which should help avoid
    uncessary verion number inflation.

--- project/build/ContentApiClient.scala
+++ project/build/ContentApiClient.scala
@@ -6,7 +6,7 @@ class ContentApiClient(info: ProjectInfo) extends DefaultProject(info) with Idea
   val joda = "joda-time" % "joda-time" % "1.6" withSources()
   val httpClient = "commons-httpclient" % "commons-httpclient" % "3.1"
 
-  val lift_json = "net.liftweb" %% "lift-json" % "2.1" withSources()
+  val lift_json = "net.liftweb" % "lift-json_2.8.0" % "2.1" withSources()
 
   val scalaTest = "org.scalatest" % "scalatest" % "1.2" % "test" withSources()
 
@@ -15,8 +15,13 @@ class ContentApiClient(info: ProjectInfo) extends DefaultProject(info) with Idea
 
   override def managedStyle = ManagedStyle.Maven
 
-  val publishTo = Resolver.file("guardian github", new File(System.getProperty("user.home")
-          + "/guardian.github.com/maven/repo-releases"))
+  val publishTo =
+    if (projectVersion.value.toString.contains("-SNAPSHOT"))
+      Resolver.file("guardian github snapshots", new File(System.getProperty("user.home")
+            + "/guardian.github.com/maven/repo-snapshots"))
+    else
+      Resolver.file("guardian github releases", new File(System.getProperty("user.home")
+            + "/guardian.github.com/maven/repo-releases"))
 
   override def packageSrcJar= defaultJarPath("-sources.jar")
   val sourceArtifact = Artifact.sources(artifactID)
