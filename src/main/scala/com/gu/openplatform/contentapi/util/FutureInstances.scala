7c490d0d713dcbd45ad9df752c4f6ae6fff3804b
Ben James <ben.james@guardian.co.uk>
2013-03-05 14:17:18 +0000

    Monad instance for scala.concurrent.Future

---
+++ src/main/scala/com/gu/openplatform/contentapi/util/FutureInstances.scala
@@ -0,0 +1,17 @@
+package com.gu.openplatform.contentapi.util
+
+import concurrent.{ExecutionContext, Future}
+import com.gu.openplatform.contentapi.ApiError
+
+object FutureInstances {
+
+  implicit def futureMonad(implicit ex: ExecutionContext): Monad[Future] = new Monad[Future] {
+
+    def point[A](a: A) = Future.successful(a)
+
+    def bind[A, B](f: (A) => Future[B]) = _ flatMap f
+
+    def fail[A](error: ApiError) = Future.failed(error)
+  }
+
+}
