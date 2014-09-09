61c8e9b60a0c4829df1602202f163d2049c64f83
Max Harlow <max@theguardian.com>
2014-09-09 16:10:12 +0100

    Use build.scala

---
+++ project/project/plugins.scala
@@ -0,0 +1,15 @@
+import sbt._
+
+object ContentApiClientPlugins extends Build {
+
+  lazy val plugins = Project(
+    id = "content-api-client-plugins",
+    base = file(".")
+  )
+  .settings(
+    addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3"),
+    addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.2"),
+    addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
+  )
+
+}
