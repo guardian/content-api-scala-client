743c7ad653f15d2e5cd947588b1d48f770b93bdc
Adam Fisher <adam.fisher@guardian.co.uk>
2014-12-30 12:52:06 +0000

    Strip tests for removed features

--- src/test/scala/com.gu.contentapi.client/GuardianContentClientTest.scala
+++ src/test/scala/com.gu.contentapi.client/GuardianContentClientTest.scala
@@ -12,18 +12,18 @@ class GuardianContentClientTest extends FlatSpec with Matchers with ClientTest w
 
   implicit def executionContext = ExecutionContext.global
   implicit override val patienceConfig = PatienceConfig(timeout = Span(2, Seconds))
-  import api.implicits._
 
   "client interface" should "successfully call the Content API" in {
+    val query = ItemQuery().itemId("commentisfree/2012/aug/01/cyclists-like-pedestrians-must-get-angry")
     val content = for {
-      response <- api.item.itemId("commentisfree/2012/aug/01/cyclists-like-pedestrians-must-get-angry").response
+      response <- api.getResponse(query)
     } yield response.content.get
     content.futureValue.id should be ("commentisfree/2012/aug/01/cyclists-like-pedestrians-must-get-angry")
   }
 
   it should "return errors as a broken promise" in {
-    val errorResponse = api.item.itemId("something-that-does-not-exist").response
-    val errorTest = errorResponse recover { case error =>
+    val query = ItemQuery().itemId("something-that-does-not-exist")
+    val errorTest = api.getResponse(query) recover { case error =>
       error should be (GuardianContentApiError(404, "Not Found"))
     }
     errorTest.futureValue
@@ -54,19 +54,9 @@ class GuardianContentClientTest extends FlatSpec with Matchers with ClientTest w
     content.futureValue.id should be ("commentisfree/2012/aug/01/cyclists-like-pedestrians-must-get-angry")
   }
 
-  it should "support itemQuery's apiUrl" in {
-    val query = ItemQuery().apiUrl(s"${api.targetUrl}/commentisfree/2012/aug/01/cyclists-like-pedestrians-must-get-angry")
-    query.id.value should equal("commentisfree/2012/aug/01/cyclists-like-pedestrians-must-get-angry")
-  }
-
   it should "perform a given collection query" in {
     val query = CollectionQuery().collectionId("uk-alpha/news/regular-stories")
     val collection = for (response <- api.getResponse(query)) yield response.collection
     collection.futureValue.id should equal("uk-alpha/news/regular-stories")
-  }
-
-  it should "support collectionQuery's apiUrl method" in {
-    val query = CollectionQuery().apiUrl(s"${api.targetUrl}/collections/41cc-b696-db2e-0156")
-    query.collectionId.value should equal("41cc-b696-db2e-0156")
   }
 }
