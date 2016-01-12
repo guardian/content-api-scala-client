410cbd4ca4c91e049eeac2f532dcbcb44663a74d
Paul Roberts <paul.roberts@guardian.co.uk>
2016-01-12 19:53:02 +0000

    add the content atoms model to the response

--- models/src/main/thrift/content/v1.thrift
+++ models/src/main/thrift/content/v1.thrift
@@ -1,5 +1,7 @@
 namespace scala com.gu.contentapi.client.model.v1
 
+include "quiz.thrift"
+
 struct CapiDateTime {
 
     /*
@@ -875,6 +877,18 @@ struct Tag {
     17: optional string twitterHandle
 }
 
+struct QuizAtom {
+  1: required string id
+  2: required list<string> labels // required, but may be empty
+  3: required string defaultHtml
+  4: required quiz.QuizAtomData data       // the atom payload
+//  6: required ContentChangeDetails contentChangeDetails
+ }
+
+/* an embedded content item with an independant lifecycle */
+struct Atoms {
+   1: optional QuizAtom quiz
+}
 
 struct Content {
 
@@ -977,6 +991,8 @@ struct Content {
     15: optional Rights rights
 
     16: optional Crossword crossword
+
+    17: optional Atoms atoms
 }
 
 struct Edition {
