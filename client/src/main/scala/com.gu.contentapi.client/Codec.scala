f7ca998697efc62ff3ae6beee2aca16cd4f9118c
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-01-14 09:34:18 +0000

    Factor out deserialization and unify API

---
+++ client/src/main/scala/com.gu.contentapi.client/Codec.scala
@@ -0,0 +1,83 @@
+package com.gu.contentapi.client
+
+import com.gu.contentapi.client.model._
+import com.gu.contentapi.client.model.v1._
+import com.gu.contentapi.client.thrift.ThriftDeserializer
+
+private[client] trait Codec[Q] {
+  type R
+  def decode: Array[Byte] => R
+}
+
+private[client] object Codec {
+  implicit val item = new Codec[ItemQuery] {
+    type R = ItemResponse
+    def decode = ThriftDeserializer.deserialize(_, ItemResponse)
+  }
+
+  implicit val searchQuery = new Codec[SearchQuery] {
+    type R = SearchResponse
+    def decode = ThriftDeserializer.deserialize(_, SearchResponse)
+  }
+
+  implicit val tagsQuery = new Codec[TagsQuery] {
+    type R = TagsResponse
+    def decode = ThriftDeserializer.deserialize(_, TagsResponse)
+  }
+
+  implicit val sectionsQuery = new Codec[SectionsQuery] {
+    type R = SectionsResponse
+    def decode = ThriftDeserializer.deserialize(_, SectionsResponse)
+  }
+
+  implicit val editionsQuery = new Codec[EditionsQuery] {
+    type R = EditionsResponse
+    def decode = ThriftDeserializer.deserialize(_, EditionsResponse)
+  }
+
+  implicit val removedContentQuery = new Codec[RemovedContentQuery] {
+    type R = RemovedContentResponse
+    def decode = ThriftDeserializer.deserialize(_, RemovedContentResponse)
+  }
+
+  implicit val videoStatsQuery = new Codec[VideoStatsQuery] {
+    type R = VideoStatsResponse
+    def decode = ThriftDeserializer.deserialize(_, VideoStatsResponse)
+  }
+
+  implicit val atomsQuery = new Codec[AtomsQuery] {
+    type R = AtomsResponse
+    def decode = ThriftDeserializer.deserialize(_, AtomsResponse)
+  }
+
+  implicit val recipesQuery = new Codec[RecipesQuery] {
+    type R = AtomsResponse
+    def decode = ThriftDeserializer.deserialize(_, AtomsResponse)
+  }
+
+  implicit val reviewsQuery = new Codec[ReviewsQuery] {
+    type R = AtomsResponse
+    def decode = ThriftDeserializer.deserialize(_, AtomsResponse)
+  }
+
+  implicit val gameReviewsQuery = new Codec[GameReviewsQuery] {
+    type R = AtomsResponse
+    def decode = ThriftDeserializer.deserialize(_, AtomsResponse)
+  }
+
+  implicit val restaurantReviewsQuery = new Codec[RestaurantReviewsQuery] {
+    type R = AtomsResponse
+    def decode = ThriftDeserializer.deserialize(_, AtomsResponse)
+  }
+
+  implicit val filmReviewsQuery = new Codec[FilmReviewsQuery] {
+    type R = AtomsResponse
+    def decode = ThriftDeserializer.deserialize(_, AtomsResponse)
+  }
+
+  implicit val storiesQuery = new Codec[StoriesQuery] {
+    type R = StoriesResponse
+    def decode = ThriftDeserializer.deserialize(_, StoriesResponse)
+  }
+
+}
--- client/src/main/scala/com.gu.contentapi.client/GuardianContentClient.scala
+++ client/src/main/scala/com.gu.contentapi.client/GuardianContentClient.scala
@@ -21,22 +21,6 @@ trait ContentApiClientLogic {
 
   val targetUrl = "https://content.guardianapis.com"
 
-  def item(id: String) = ItemQuery(id)
-  val search = SearchQuery()
-  val tags = TagsQuery()
-  val sections = SectionsQuery()
-  val editions = EditionsQuery()
-  val removedContent = RemovedContentQuery()
-  val atoms = AtomsQuery()
-  val recipes = RecipesQuery()
-  val reviews = ReviewsQuery()
-  val gameReviews = GameReviewsQuery()
-  val restaurantReviews = RestaurantReviewsQuery()
-  val filmReviews = FilmReviewsQuery()
-  val videoStats = VideoStatsQuery()
-  val stories = StoriesQuery()
-
-
   protected[client] def url(location: String, parameters: Map[String, String]): String = {
     require(!location.contains('?'), "must not specify parameters in URL")
 
@@ -68,76 +52,29 @@ trait ContentApiClientLogic {
 
   /* Exposed API */
 
-  def getResponse(itemQuery: ItemQuery)(implicit context: ExecutionContext): Future[ItemResponse] =
-    fetchResponse(itemQuery) map { response =>
-      ThriftDeserializer.deserialize(response, ItemResponse)
-    }
+  import Codec._
 
-  def getResponse(searchQuery: SearchQuery)(implicit context: ExecutionContext): Future[SearchResponse] =
-    fetchResponse(searchQuery) map { response =>
-      ThriftDeserializer.deserialize(response, SearchResponse)
-    }
+  def item(id: String) = ItemQuery(id)
+  val search = SearchQuery()
+  val tags = TagsQuery()
+  val sections = SectionsQuery()
+  val editions = EditionsQuery()
+  val removedContent = RemovedContentQuery()
+  val atoms = AtomsQuery()
+  val recipes = RecipesQuery()
+  val reviews = ReviewsQuery()
+  val gameReviews = GameReviewsQuery()
+  val restaurantReviews = RestaurantReviewsQuery()
+  val filmReviews = FilmReviewsQuery()
+  val videoStats = VideoStatsQuery()
+  val stories = StoriesQuery()
 
-  def getResponse(tagsQuery: TagsQuery)(implicit context: ExecutionContext): Future[TagsResponse] =
-    fetchResponse(tagsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, TagsResponse)
-    }
-
-  def getResponse(sectionsQuery: SectionsQuery)(implicit context: ExecutionContext): Future[SectionsResponse] =
-    fetchResponse(sectionsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, SectionsResponse)
-    }
-
-  def getResponse(editionsQuery: EditionsQuery)(implicit context: ExecutionContext): Future[EditionsResponse] =
-    fetchResponse(editionsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, EditionsResponse)
-    }
-
-  def getResponse(removedContentQuery: RemovedContentQuery)(implicit context: ExecutionContext): Future[RemovedContentResponse] =
-    fetchResponse(removedContentQuery) map { response =>
-      ThriftDeserializer.deserialize(response, RemovedContentResponse)
-    }
-
-  def getResponse(videoStatsQuery: VideoStatsQuery)(implicit context: ExecutionContext): Future[VideoStatsResponse] =
-    fetchResponse(videoStatsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, VideoStatsResponse)
-    }
-
-  def getResponse(atomsQuery: AtomsQuery)(implicit context: ExecutionContext): Future[AtomsResponse] =
-    fetchResponse(atomsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, AtomsResponse)
-    }
-
-  def getResponse(recipesQuery: RecipesQuery)(implicit context: ExecutionContext): Future[AtomsResponse] =
-    fetchResponse(recipesQuery) map { response =>
-      ThriftDeserializer.deserialize(response, AtomsResponse)
-    }
-
-  def getResponse(reviewsQuery: ReviewsQuery)(implicit context: ExecutionContext): Future[AtomsResponse] =
-    fetchResponse(reviewsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, AtomsResponse)
-    }
-
-  def getResponse(gameReviewsQuery: GameReviewsQuery)(implicit context: ExecutionContext): Future[AtomsResponse] =
-    fetchResponse(gameReviewsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, AtomsResponse)
-    }
-
-  def getResponse(restaurantReviewsQuery: RestaurantReviewsQuery)(implicit context: ExecutionContext): Future[AtomsResponse] =
-    fetchResponse(restaurantReviewsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, AtomsResponse)
-    }
-
-  def getResponse(filmReviewsQuery: FilmReviewsQuery)(implicit context: ExecutionContext): Future[AtomsResponse] =
-    fetchResponse(filmReviewsQuery) map { response =>
-      ThriftDeserializer.deserialize(response, AtomsResponse)
-    }
-
-  def getResponse(storiesQuery: StoriesQuery)(implicit context: ExecutionContext): Future[StoriesResponse] =
-    fetchResponse(storiesQuery) map { response =>
-      ThriftDeserializer.deserialize(response, StoriesResponse)
-    }
-
+  def getResponse[Q <: ContentApiQuery](q: Q)(
+    implicit 
+    context: ExecutionContext,
+    codec: Codec[Q]
+  ): Future[codec.R] =
+    fetchResponse(q) map codec.decode
 }
 
 class GuardianContentClient(val apiKey: String) extends ContentApiClientLogic {
