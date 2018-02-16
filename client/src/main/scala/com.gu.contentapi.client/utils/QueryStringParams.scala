eea6de53c62c56128ffd06aeecb6f7a064ada842
tomrf1 <tom.forbes@theguardian.com>
2018-02-16 15:09:10 +0000

    Remove unnecessary match

--- client/src/main/scala/com.gu.contentapi.client/utils/QueryStringParams.scala
+++ client/src/main/scala/com.gu.contentapi.client/utils/QueryStringParams.scala
@@ -4,13 +4,11 @@ import java.net.URLEncoder
 
 object QueryStringParams {
   def apply(parameters: Iterable[(String, String)]) = {
-    def encodeParameter(p: String): String = p match {
-      /**
-        * api-gateway IAM authorisation requires that spaces are encoded as `%20`, not `+`.
-        * https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-        */
-      case other => URLEncoder.encode(other, "UTF-8").replace("+", "%20")
-    }
+    /**
+      * api-gateway IAM authorisation requires that spaces are encoded as `%20`, not `+`.
+      * https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+      */
+    def encodeParameter(p: String): String = URLEncoder.encode(p, "UTF-8").replace("+", "%20")
 
     if (parameters.isEmpty) {
       ""
