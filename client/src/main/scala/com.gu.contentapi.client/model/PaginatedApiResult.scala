63f45549945b482f1ba60970a35839860ad68046
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-03-23 14:43:33 +0000

    - Rename file - Remove type Result

--- client/src/main/scala/com.gu.contentapi.client/model/PaginatedApiResult.scala
+++ client/src/main/scala/com.gu.contentapi.client/model/PaginatedApiResult.scala
@@ -4,7 +4,6 @@ import com.gu.contentapi.client.model.v1._
 import com.gu.contentatom.thrift.Atom
 
 trait PaginatedApiResult[Response] {
-  type Result
   def getNextId: Response => Option[String]
   def getPrevId: Response => Option[String]
 }
@@ -12,25 +11,21 @@ trait PaginatedApiResult[Response] {
 object PaginatedApiResult {
 
   implicit val searchResponse = new PaginatedApiResult[SearchResponse] {
-    type Result = Content
     def getNextId = r => if (r.pages == r.currentPage) None else r.results.lastOption.map(_.id)
     def getPrevId = r => if (1 == r.currentPage) None else r.results.headOption.map(_.id)
   }
 
   implicit val itemResponse = new PaginatedApiResult[ItemResponse] {
-    type Result = Content
     def getNextId = r => if (r.pages.exists(ps => r.currentPage.exists(cp => ps == cp))) None else r.results.flatMap(_.lastOption.map(_.id))
     def getPrevId = r => if (r.currentPage.exists(cp => 1 == cp)) None else r.results.flatMap(_.lastOption.map(_.id))
   }
 
   implicit val tagsResponse = new PaginatedApiResult[TagsResponse] {
-    type Result = Tag
     def getNextId = r => if (r.pages == r.currentPage) None else r.results.lastOption.map(_.id)
     def getPrevId = r => if (1 == r.currentPage) None else r.results.headOption.map(_.id)
   }
 
   implicit val atomsResponse = new PaginatedApiResult[AtomsResponse] {
-    type Result = Atom
     def getNextId = r => if (r.pages == r.currentPage) None else r.results.lastOption.map(_.id)
     def getPrevId = r => if (1 == r.currentPage) None else r.results.headOption.map(_.id)
   }
