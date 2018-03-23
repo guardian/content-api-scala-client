1019a08e6a98ce6a76a32e66ffbfabc714629811
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-03-23 13:15:55 +0000

    Nice! Sets are functions A => Boolean :-)

--- client/src/main/scala/com.gu.contentapi.client/model/package.scala
+++ client/src/main/scala/com.gu.contentapi.client/model/package.scala
@@ -5,5 +5,5 @@ package object model {
 
   private[model] def not[A](f: A => Boolean): A => Boolean = !f(_)
 
-  private[model] def isPaginationParameter: String => Boolean = List("page", "page-size").contains(_)
+  private[model] val isPaginationParameter = Set("page", "page-size")
 }
