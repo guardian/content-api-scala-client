aadbac57c4ac0797ba4d0a1f07d1193d4932a0d8
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-03-28 12:22:16 +0100

    Rename for clarity

--- client/src/main/scala/com.gu.contentapi.client/model/HttpResponse.scala
+++ client/src/main/scala/com.gu.contentapi.client/model/HttpResponse.scala
@@ -5,10 +5,10 @@ import scala.concurrent.Future
 case class HttpResponse(body: Array[Byte], statusCode: Int, statusMessage: String)
 
 object HttpResponse {
-  val isValid = Set(200, 302) 
+  val isSuccessful = Set(200, 302) 
   
   def check: HttpResponse => Future[Array[Byte]] = {
-    case HttpResponse(body, statusCode, _) if isValid(statusCode) => Future.successful(body)
+    case HttpResponse(body, statusCode, _) if isSuccessful(statusCode) => Future.successful(body)
     case response => Future.failed(ContentApiError(response))
   }
 }
