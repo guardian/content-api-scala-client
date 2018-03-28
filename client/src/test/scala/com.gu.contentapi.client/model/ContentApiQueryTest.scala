b6b7ce7305acbd59c72ebc87f0914bffcc41c225
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-03-28 15:27:41 +0100

    Fix test after changing toStrign

--- client/src/test/scala/com.gu.contentapi.client/model/ContentApiQueryTest.scala
+++ client/src/test/scala/com.gu.contentapi.client/model/ContentApiQueryTest.scala
@@ -5,27 +5,27 @@ import org.scalatest.{Matchers, FlatSpec}
 class ContentApiQueryTest extends FlatSpec with Matchers  {
   "ItemQuery.toString" should "be excellent" in {
     ItemQuery("profile/robert-berry").showFields("all").toString shouldEqual
-      "ItemQuery(/profile/robert-berry?show-fields=all)"
+      "/profile/robert-berry?show-fields=all"
   }
 
   "SearchQuery.toString" should "also be excellent" in {
     SearchQuery().tag("profile/robert-berry").showElements("all").contentType("article").toString shouldEqual
-      "SearchQuery(/search?tag=profile%2Frobert-berry&show-elements=all&type=article)"
+      "/search?tag=profile%2Frobert-berry&show-elements=all&type=article"
   }
 
   "SectionsQuery.toString" should "be beautiful" in {
-    SectionsQuery().toString shouldEqual "SectionsQuery(/sections)"
+    SectionsQuery().toString shouldEqual "/sections"
   }
 
   "SectionsQuery.toString" should "add sponsorship-type filter" in {
-    SectionsQuery().sponsorshipType("paid-content").toString shouldEqual "SectionsQuery(/sections?sponsorship-type=paid-content)"
+    SectionsQuery().sponsorshipType("paid-content").toString shouldEqual "/sections?sponsorship-type=paid-content"
   }
 
   "TagsQuery.toString" should "be awesome" in {
-    TagsQuery().tagType("contributor").toString shouldEqual "TagsQuery(/tags?type=contributor)"
+    TagsQuery().tagType("contributor").toString shouldEqual "/tags?type=contributor"
   }
 
   "RemovedContentQuery.toString" should "be amazing" in {
-    RemovedContentQuery().reason("gone").toString shouldEqual "RemovedContentQuery(/content/removed?reason=gone)"
+    RemovedContentQuery().reason("gone").toString shouldEqual "/content/removed?reason=gone"
   }
 }
