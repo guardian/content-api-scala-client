2e2433b16625a2f1932a80d944bd90b09a93cef9
Susie Coleman <susiecoleman@users.noreply.github.com>
2018-09-19 16:32:36 +0100

    Bump capi models version. (#279)
    * Bump capi models version.
    
    * Update changelog.
    
    * Bump capi models to 12.10
    
    * More informative CHANGELOG.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val scalaVersions = Seq("2.11.12", "2.12.6")
 
-  val CapiModelsVersion = "12.8"
+  val CapiModelsVersion = "12.10"
 
   val clientDeps = Seq(
     "com.gu" %% "content-api-models-scala" % CapiModelsVersion,
