2df75c4ae30d96d4c835b6e083ce8a69308d5d10
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-03-19 18:14:42 +0000

    Bump dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val scalaVersions = Seq("2.11.12", "2.12.8")
 
-  val CapiModelsVersion = "12.15"
+  val CapiModelsVersion = "12.18"
 
   val clientDeps = Seq(
     "com.gu" %% "content-api-models-scala" % CapiModelsVersion,
