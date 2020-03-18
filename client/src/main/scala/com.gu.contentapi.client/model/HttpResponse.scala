e7958ed5c17c81de1701d5f9dbaf75eba1060b6d
Justin Pinner <justinpinner@gmail.com>
2020-03-18 17:45:54 +0000

    Don't retry 503s

--- client/src/main/scala/com.gu.contentapi.client/model/HttpResponse.scala
+++ client/src/main/scala/com.gu.contentapi.client/model/HttpResponse.scala
@@ -3,7 +3,7 @@ package com.gu.contentapi.client.model
 case class HttpResponse(body: Array[Byte], statusCode: Int, statusMessage: String)
 
 object HttpResponse {
-  val failedButMaybeRecoverableCodes = Set(408, 429, 503, 504, 509)
+  val failedButMaybeRecoverableCodes = Set(408, 429, 504, 509)
   val successfulResponseCodes = Set(200, 302)
 
   def isSuccessHttpResponse(httpResponse: HttpResponse) = successfulResponseCodes.contains(httpResponse.statusCode)
