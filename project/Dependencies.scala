9cbc77374f0c40c89c7ffd3723bb417f9eee26b9
Justin Pinner <justinpinner@gmail.com>
2021-04-21 11:57:14 +0100

    Import capi models 16.0.0-RC2 and upgrade thrift to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,13 @@ import sbt._
 object Dependencies {
   val scalaVersions = Seq("2.11.12", "2.12.10", "2.13.1")
 
-  val CapiModelsVersion = "15.10.2"
+  val CapiModelsVersion = "16.0.0-RC2"
 
+  // Note: keep libthrift at a version functionally compatible with that used in CAPI models
+  // if build failures occur due to eviction / sbt-assembly mergeStrategy errors
   val clientDeps = Seq(
     "com.gu" %% "content-api-models-scala" % CapiModelsVersion,
-    "org.apache.thrift" % "libthrift" % "0.12.0",
+    "org.apache.thrift" % "libthrift" % "0.13.0",
     "org.scalatest" %% "scalatest" % "3.0.8" % "test" exclude("org.mockito", "mockito-core"),
     "org.slf4j" % "slf4j-api" % "1.7.25",
     "org.mockito" % "mockito-all" % "1.10.19" % "test"
