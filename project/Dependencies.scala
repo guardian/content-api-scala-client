621e992e9eb5613be6fde44a63c1b8929bad77b6
Andy Gallagher <andy.gallagher@theguardian.com>
2023-03-16 11:08:30 +0000

    version 19.2.2-SNAPSHOT released

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val scalaVersions = Seq("2.13.9")
+  val scalaVersions = Seq("2.12.10", "2.13.9")
   val capiModelsVersion = "17.5.1"
   val thriftVersion = "0.15.0"
   val commonsCodecVersion = "1.10"
