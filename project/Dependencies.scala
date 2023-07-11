ecd66f29146c8b3a21c441a59bedc5975109daee
Sophie <sophiemacmillan93@gmail.com>
2023-07-11 20:01:21 +0100

    Bump version of content-api-models

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalaVersions = Seq("2.12.10", "2.13.1")
-  val capiModelsVersion = "17.6.2"
+  val capiModelsVersion = "17.7.4-SNAPSHOT"
   val thriftVersion = "0.15.0"
   val commonsCodecVersion = "1.10"
   val scalaTestVersion = "3.0.8"
