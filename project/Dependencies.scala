49e32db9531d7cba224a44cdf5cb22f3f76ad04b
frederickobrien <frederick.obrien@guardian.co.uk>
2024-03-05 10:34:37 +0000

    Bump preview version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalaVersions = Seq("2.12.19", "2.13.13")
-  val capiModelsVersion = "21.0.0-PREVIEW.agschema-org-field.2024-03-01T1602.c5a81993"
+  val capiModelsVersion = "21.0.0-PREVIEW.agschema-org-field.2024-03-05T1018.a555b63a"
   val thriftVersion = "0.19.0"
   val commonsCodecVersion = "1.16.1"
   val scalaTestVersion = "3.0.9"
