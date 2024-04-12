e90e3ceed2a8d8264af26fc663f5622db999b938
Divya Bhatt <divya.bhatt@guardian.co.uk>
2024-04-12 13:54:34 +0100

    Bump production release for content-api-model

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalaVersions = Seq("2.12.19", "2.13.13")
-  val capiModelsVersion = "23.0.0"
+  val capiModelsVersion = "24.0.0"
   val thriftVersion = "0.19.0"
   val commonsCodecVersion = "1.16.1"
   val scalaTestVersion = "3.0.9"
