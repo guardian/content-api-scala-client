001cc6df0ca22c1fc627b0c2fc31066a5031d18c
Oliver Abrahams <ollie.abrahams@guardian.co.uk>
2025-10-31 10:07:47 +0000

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalaVersions = Seq("2.12.19", "2.13.14")
-  val capiModelsVersion = "32.0.0-PREVIEW.filterproduct-element-temp.2025-10-17T1357.19edb5e8"
+  val capiModelsVersion = "32.0.0"
   val thriftVersion = "0.20.0"
   val commonsCodecVersion = "1.17.0"
   val scalaTestVersion = "3.2.18"
