497667cfcc813095cb5c1e0f2157b49b0cbee3cf
Simon Byford <simon.byford@guardian.co.uk>
2025-11-13 10:38:34 +0000

    Use non-preview release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalaVersions = Seq("2.12.19", "2.13.14")
-  val capiModelsVersion = "32.0.0-PREVIEW.bump-content-atom-to-support-looping-video-field.2025-10-29T1136.98f8ace6"
+  val capiModelsVersion = "33.0.0"
   val thriftVersion = "0.20.0"
   val commonsCodecVersion = "1.17.0"
   val scalaTestVersion = "3.2.18"
