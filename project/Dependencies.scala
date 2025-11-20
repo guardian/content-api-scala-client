92584f333793d577cd7e60cc34673bb01f603e73
Andy Noton <andy.noton.contractor@guardian.co.uk>
2025-11-20 11:16:33 +0000

    update to release version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalaVersions = Seq("2.12.19", "2.13.14")
-  val capiModelsVersion = "34.0.0-PREVIEW.anbump-content-atom-model.2025-11-17T0928.91d3cce9"
+  val capiModelsVersion = "34.0.0"
   val thriftVersion = "0.20.0"
   val commonsCodecVersion = "1.17.0"
   val scalaTestVersion = "3.2.18"
