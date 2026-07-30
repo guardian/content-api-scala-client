d0c6312ea71bfe14ff4e2345fccff5564b245218
Roberto Tyley <roberto.tyley@gmail.com>
2026-07-30 15:52:57 +0100

    Add Cats Effect CAPI client
    Add logging and re-enable retries

---
+++ client-cats-effect/src/main/scala/com/gu/contentapi/client/catseffect/bulkqueries/BulkContentRequester.scala
@@ -0,0 +1,67 @@
+package com.gu.contentapi.client.catseffect.bulkqueries
+
+import cats.effect.IO
+import com.gu.contentapi.client.catseffect.IOCapiClient
+import com.gu.contentapi.client.catseffect.bulkqueries.RequestSizing.Calculated.{availableUrlSpaceForIdsGiven, capiIdSizer}
+import com.gu.contentapi.client.catseffect.bulkqueries.RequestSizing.MaxItemsAllowedInIdsParameter
+import com.gu.contentapi.client.model.v1.Content
+import com.gu.contentapi.client.model.{CapiId, SearchQuery}
+import com.madgag.algo.packing.binpacking.BinPacking.Size.CardinalityConstrained
+import com.madgag.algo.packing.binpacking.OfflineAlgorithm.{FFD, dotProductFFD}
+import com.madgag.algo.packing.binpacking._
+import fs2.Stream
+import fs2.Stream.emits
+import spire.implicits._
+
+/**
+ * This class provides safe bulk-requesting of `Content` by `CapiId`.
+ *
+ * When requesting an arbitrary number of `CapiId`s, a single request can easily exceed one of two hard limits:
+ *
+ *   - the url can grow larger than the 4096 character '''url-size limit''' imposed by the Content API HTTP endpoints,
+ *     and get a [[https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/414 HTTP 414 URI Too Long]] error
+ *     response.
+ *   - the ids parameter itself has a '''max-ids-per-request limit''' of 50 CAPI ids.
+ *
+ * This class works out how much url-space is available, and [[https://github.com/rtyley/bin-packing packs]] the
+ * requested `CapiId`s into the lowest-possible number of requests, respecting both the url-size and
+ * max-ids-per-request limits (sometimes, CAPI ids _can_ be short enough to fit more than 50 into the request url -
+ * exceeding the max-ids-per-request limit but not the url-size limit).
+ *
+ * Creating an instance of `BulkContentRequester` performs some initial space calculations. You can retain the
+ * instance and then use it over and over again with different sets of CAPI ids.
+ */
+class BulkContentRequester private(client: IOCapiClient, baseQuery: SearchQuery) {
+
+  private val packer: Packer[CapiId] = Packer[CardinalityConstrained](
+    binSize = CardinalityConstrained(
+      size = availableUrlSpaceForIdsGiven(client.underlyingClient, baseQuery),
+      cardinality = MaxItemsAllowedInIdsParameter
+    ),
+    dotProductFFD(CardinalityConstrained.coordSpace)
+  ).using[CapiId](capiId => CardinalityConstrained.item(capiIdSizer(capiId)))
+
+  /** @param capiIds
+    *   The set of CAPI-ids can be large - it is not constrained by URL-length
+    */
+  def contentStream(capiIds: Set[CapiId]): Stream[IO, Content] =
+    emits(pack(capiIds).toSeq).parEvalMapUnorderedUnbounded(bulkGet).flatMap(emits)
+
+  /** @return
+   *   CAPI-ids grouped so that the query will not exceed either the url-size or max-ids-per-request limits
+   */
+  private def pack(capiIds: Set[CapiId]): Set[Set[CapiId]] = capiIds.packWith(packer)
+
+  /** Note: This method does not check any size limits - if the set of `CapiId`s is too large, the
+   * Content API service will return an HTTP error response.
+   */
+  private def bulkGet(capiIds: Set[CapiId]): IO[Seq[Content]] =
+    client.getResponse(baseQuery.withIds(capiIds)).map(_.results.toSeq)
+}
+
+object BulkContentRequester {
+  def apply(client: IOCapiClient, baseQuery: SearchQuery): BulkContentRequester = new BulkContentRequester(
+    client,
+    baseQuery.pageSize(MaxItemsAllowedInIdsParameter) // default page-size is 10, too small - we can pack more CAPI ids than that into a request
+  )
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,6 @@ object Dependencies {
   val slf4jVersion = "2.0.18"
   val mockitoVersion = "5.12.0"
   val okhttpVersion = "4.12.0"
-  val awsSdkVersion = "1.11.280"
 
   // Note: keep libthrift at a version functionally compatible with that used in content-api-models
   // if build failures occur due to eviction / sbt-assembly mergeStrategy errors
@@ -17,7 +16,7 @@ object Dependencies {
     "com.gu" %% "content-api-models-scala" % capiModelsVersion,
     "org.apache.thrift" % "libthrift" % thriftVersion,
     "commons-codec" % "commons-codec" % commonsCodecVersion,
-    "org.scalatest" %% "scalatest" % scalaTestVersion % "test" exclude("org.mockito", "mockito-core"),
+    "org.scalatest" %% "scalatest" % scalaTestVersion % "test" `exclude`("org.mockito", "mockito-core"),
     "org.scalatestplus" %% "mockito-4-11" % "3.2.18.0" % "test",
     "org.slf4j" % "slf4j-api" % slf4jVersion,
     "org.mockito" % "mockito-core" % mockitoVersion
